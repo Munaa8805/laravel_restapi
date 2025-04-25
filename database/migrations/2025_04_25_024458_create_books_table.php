d63c0a112cc61c6724eb89ecae6132de606b503a
Munaa8805 <munaatsetsegmaa@Munaas-MacBook-Pro.local>
2025-04-24 21:10:34 -0600

    create controllers

---
+++ database/migrations/2025_04_25_024458_create_books_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->string('name');
+            $table->string('author');
+            $table->string('isbn')->unique();
+            $table->date('publication_date');
+            $table->string('genre');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\Api\AttendeeController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+Route::apiResource('books', BookController::class);
+Route::apiResource('books.attendees', AttendeeController::class)
+    ->scoped(['attendee' => 'book']);
